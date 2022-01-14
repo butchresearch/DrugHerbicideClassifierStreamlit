a8a815c50ce49edbf70f8c8561c457ca91850c06
tacos <sagrario3098@gmail.com>
2022-01-14 00:27:51 -0600

    Paginator

--- main.py
+++ main.py
@@ -17,6 +17,57 @@ st.write(
         
         """)
 
+def paginator(label, items, items_per_page=20, on_sidebar=True):
+    """Lets the user paginate a set of items.
+    Parameters
+    ----------
+    label : str
+        The label to display over the pagination widget.
+    items : Iterator[Any]
+        The items to display in the paginator.
+    items_per_page: int
+        The number of items to display per page.
+    on_sidebar: bool
+        Whether to display the paginator widget on the sidebar.
+        
+    Returns
+    -------
+    Iterator[Tuple[int, Any]]
+        An iterator over *only the items on that page*, including
+        the item's index.
+    Example
+    -------
+    This shows how to display a few pages of fruit.
+    >>> fruit_list = [
+    ...     'Kiwifruit', 'Honeydew', 'Cherry', 'Honeyberry', 'Pear',
+    ...     'Apple', 'Nectarine', 'Soursop', 'Pineapple', 'Satsuma',
+    ...     'Fig', 'Huckleberry', 'Coconut', 'Plantain', 'Jujube',
+    ...     'Guava', 'Clementine', 'Grape', 'Tayberry', 'Salak',
+    ...     'Raspberry', 'Loquat', 'Nance', 'Peach', 'Akee'
+    ... ]
+    ...
+    ... for i, fruit in paginator("Select a fruit page", fruit_list):
+    ...     st.write('%s. **%s**' % (i, fruit))
+    """
+
+    # Figure out where to display the paginator
+    if on_sidebar:
+        location = st.sidebar.empty()
+    else:
+        location = st.empty()
+
+    # Display a pagination selectbox in the specified location.
+    items = list(items)
+    n_pages = len(items)
+    n_pages = (len(items) - 1) // items_per_page + 1
+    page_format_func = lambda i: "Page %s" % i
+    page_number = location.selectbox(label, range(n_pages), format_func=page_format_func)
+
+    # Iterate over the items in the page to let the user display them.
+    min_index = page_number * items_per_page
+    max_index = min_index + items_per_page
+    import itertools
+    return itertools.islice(enumerate(items), min_index, max_index)
 
 @st.cache
 def fetch_and_clean_data(data):
@@ -57,19 +108,19 @@ if user_input != None:
 
        # mols = [Chem.MolFromSmiles(i) for i in user_input] 
         #st.image(Draw.MolsToGridImage(mols))
-
+        st.write(
+        """
+        ## Molecule Visualizer
+        
+        """)
         if type(user_input) == str:
                 m = Chem.MolFromSmiles(user_input)
                 im= Draw.MolToImage(m)
-                st.image(im)
+                st.image(im,caption=user_input)
 
         elif  type(user_input) == list:
                 mols = [Chem.MolFromSmiles(i) for i in user_input] 
-                for m in mols:
-                        im= Draw.MolToImage(m)
-                        st.image(im)
-
-             
-
-
-      
+                ims  = [Draw.MolToImage(i) for i in mols]
+                image_iterator = paginator("Molecules", ims)
+                indices_on_page, images_on_page = map(list, zip(*image_iterator))
+                st.image(images_on_page, width=100, caption=indices_on_page)
